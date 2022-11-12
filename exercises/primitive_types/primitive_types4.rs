83c44d36c738ba9a50ddeb329f19ee658456c50d
Yijing Huang <hyj891204@gmail.com>
2022-11-11 18:00:33 -0600

    primitive_types"

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -2,13 +2,12 @@
 // Get a slice out of Array a where the ??? is so that the test passes.
 // Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    let nice_slice = &a[1..4];
+    // nice_slice 存的是个指针，里面指向index 1-3,是不包括index 4的数据（也就是最后一个5）
 
     assert_eq!([2, 3, 4], nice_slice)
 }
--- exercises/primitive_types/primitive_types5.rs
+++ exercises/primitive_types/primitive_types5.rs
@@ -2,11 +2,9 @@
 // Destructure the `cat` tuple so that the println will work.
 // Execute `rustlings hint primitive_types5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
     let cat = ("Furry McFurson", 3.5);
-    let /* your pattern here */ = cat;
-
+    let (name, age) = cat;
+    // 用这个方法提取tuple里面的数据显示出来
     println!("{} is {} years old.", name, age);
 }
