05fb93a61e016a4d6b442257450849f6532310f3
Yijing Huang <hyj891204@gmail.com>
2022-11-15 17:40:08 -0600

    move

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -7,19 +7,20 @@
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    get_char(&data);
 
-    string_uppercase(&data);
+    string_uppercase(data);
 }
 
 // Should not take ownership
-fn get_char(data: String) -> char {
+fn get_char(data: &String) -> char {
+    //unwrap 是一种快捷写法， 意思是用match检查有没有这个值，有的话就返回里面的值，没有的话就返回一个panic
     data.chars().last().unwrap()
 }
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+fn string_uppercase(mut data: String) {
+    data = data.to_uppercase(); //to_uppercase 是返回一个新值，而不是在原来的值上面修改
 
     println!("{}", data);
 }
